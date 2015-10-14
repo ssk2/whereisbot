a42e11577e63f2304c68a20ad46dcb01f35412a5
Sunil Shah <sunil@mesosphere.io>
2015-10-14 00:03:34 -0700

    Update message.

--- whereisbot/whereisbot.py
+++ whereisbot/whereisbot.py
@@ -8,7 +8,7 @@ source_json = os.environ["SOURCE_JSON"]
 def _get_by_date(name, date):
     locations_map = _load_locations_map(name)
     if date.date() in locations_map:
-        return locations_map[date.date()]
+        return "{0} is {1}".format(name, locations_map[date.date()]
     else:
         return "undefined"
 
@@ -18,7 +18,7 @@ def _load_locations_map(name):
     print ("Fetched locations file")
     locations_map = {}
     for person in locations_file["people"]:
-        if person["name"].lower() == name:
+        if person["name"].lower() == name.lower():
             for location in person["locations"]:
                 unparsed_date = location["date"]
                 parsed_date = datetime.datetime.strptime(unparsed_date, "%Y-%m-%d")
@@ -40,7 +40,7 @@ def whereis():
         name = request.form.get('text').lower()
     else:
         user = request.args.get('user_name')
-        name = request.args.get('text').lower()
+        name = request.args.get('text')
     print ("Fetching location for name {0} date {1} from user {2}".format(name, today, user))
     return _get_by_date(name, today)
 
